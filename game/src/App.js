3ad328be14cf1d2f4c0a54323334bb07c8a86911
samuel-eduardo <samuellino2006@gmail.com>
2023-06-24 08:52:32 -0300

    iniciando

---
+++ game/src/App.js
@@ -0,0 +1,14 @@
+import "./style.css"
+
+function App() {
+  return (
+    <div className="App">
+      <div className="interface">
+        <h3 className="jogador J1">Jogador 1</h3>
+        <h3 className="jogador J2">Jogador 1</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
